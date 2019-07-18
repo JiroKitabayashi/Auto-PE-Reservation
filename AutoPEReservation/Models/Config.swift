94cbb4aa30c8cded6f5f5588d80d0a6bec53ec31
Kaishi Nishimura <nkaishi927@gmail.com>
2019-07-18 17:12:13 +0900

    Fixed

--- AutoPEReservation/Models/Config.swift
+++ AutoPEReservation/Models/Config.swift
@@ -7,4 +7,8 @@ class Config {
     let CNS_ACCOUNT_USER_DEFAULTS_KEY = "CNS_ACCOUNT"
     let CNS_PASSWORD_USER_DEFAULTS_KEY = "CNS_PASSWORD"
     let WELLNESS_BASE_URL = URL(string: "https://wellness.sfc.keio.ac.jp/index.php")!
+    
+    func createLoginURL(withCNSAcount cnsAccount: String, cnsPassword: String) -> URL {
+        return URL(string: "\(Config.shared.WELLNESS_BASE_URL)/login=\(cnsAccount)&password=\(cnsPassword)&submit=login&page=top&mode=login&semester=20190&lang=ja&limit=9999")!
+    }
 }
